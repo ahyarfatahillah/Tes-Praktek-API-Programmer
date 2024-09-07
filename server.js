6dbcb2ace64df62cff06bac7a60f3edf0e356d84
ahyarfatahillah <ahyaredukasi@gmail.com>
2024-09-08 04:28:00 +0800

    .env

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ class Server{
     }
     start(){
         this.#expressApp.listen(port, "0.0.0.0",()=>{
-            console.log(`Listen at port ${8080}`)
+            console.log(`Listen at port ${3000}`)
         })
     }
 }
